f16865bce84bb8e072ef53d0174f2b58922015b0
MZK <1807050330@qq.com>
2023-12-13 11:27:55 +0800

    功能3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <h1>小黑子</h1>
     <h3>dsfrhgfherew</h3>
     <h1>双流机场你</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App(周)" />
     <h2>llll</h2>
     <h2>enen</h2>
     <h2>周树标</h2>
@@ -14,6 +12,7 @@
     <p>123</p>
     <h1>bjyiszd</h1>
     <p>蔡徐坤哈哈哈哈哈哈</p>
+    <p>功能3</p>
   </div>
 </template>
 
